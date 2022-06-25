ae84442ce7f132d12bcaf9cd54dcb410680afbd8
Peter Henderson <Breakend@users.noreply.github.com>
2022-06-25 13:47:31 -0700

    Update utils.py

--- bertalign/utils.py
+++ bertalign/utils.py
@@ -45,22 +45,22 @@ def _split_fa(doc_string):
 
         pattern = r'([!\.\?؟]+)[\n]*'
         tmp = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, self.add_tab, doc_string)
+        doc_string = re.sub(pattern, add_tab, doc_string)
 
         pattern = r':\n'
         tmp = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, self.add_tab, doc_string)
+        doc_string = re.sub(pattern, add_tab, doc_string)
 
         pattern = r';\n'
         tmp = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, self.add_tab, doc_string)
+        doc_string = re.sub(pattern, add_tab, doc_string)
 
         pattern = r'؛\n'
         tmp = re.findall(pattern, doc_string)
-        doc_string = re.sub(pattern, self.add_tab, doc_string)
+        doc_string = re.sub(pattern, add_tab, doc_string)
 
         pattern = r'[\n]+'
-        doc_string = re.sub(pattern, self.add_tab, doc_string)
+        doc_string = re.sub(pattern, add_tab, doc_string)
 
         for number in nums_list:
             pattern = 'floatingpointnumber'
@@ -69,7 +69,13 @@ def _split_fa(doc_string):
         doc_string = doc_string.split('\t\t')
         doc_string = [x for x in doc_string if len(x) > 0]
         return doc_string
-	
+
+    def add_tab(mystring):
+        mystring = mystring.group()  # this method return the string matched by re
+        mystring = mystring.strip(' ')  # ommiting the whitespace around the pucntuation
+        mystring = mystring.strip('\n') # ommiting the newline around the pucntuation
+        mystring = " " + mystring + "\t\t"  # adding a space after and before punctuation
+        return mystring
 	
 def _split_zh(text, limit=1000):
         sent_list = []
